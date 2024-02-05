254ba73ef2e699ff9a71e7547af150bf0fc349d1
notkoyo <kaiden01@outlook.de>
2024-02-05 14:04:23 +0000

    add router and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 // import ArticleOrganiser from "./components/Article/ArticleOrganiser";
 import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <div className="grid place-items-center h-screen">
-      
-      </div>
+      <BrowserRouter basename="/app">
+        <Routes>
+          <Route path="/" />
+        </Routes>
+      </BrowserRouter>
     </>
     
   )
