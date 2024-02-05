19338c805443be9ae0251b1268db401f1f2d0bdf
notkoyo <kaiden01@outlook.de>
2024-02-05 16:45:04 +0000

    add loading for articles

--- src/components/Article/ArticleOrganiser.jsx
+++ src/components/Article/ArticleOrganiser.jsx
@@ -4,14 +4,27 @@ import {getArticles} from "../../utils/APICalls";
 
 export default function ArticleOrganiser() {
   const [articleData, setArticleData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchArticles = async () => {
       const data = await getArticles();
       setArticleData(data);
+      setIsLoading(false);
     };
     fetchArticles();
   }, []);
+
+  
+
+  if(isLoading) {
+    return (
+      <div className="flex justify-center items-center h-48">
+        <span className="loading loading-spinner loading-lg"></span>
+        <h3 className="font-bold ml-5">Loading Articles...</h3>
+      </div>
+    )
+  }
 
   return (
     <div className="flex justify-center">
